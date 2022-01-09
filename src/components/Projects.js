31ced7f2ab64a242fd05949a3ae29b57490fbb2e
Justin Zhang <judtin@seas.upenn.edu>
2022-01-09 00:35:02 -0500

    fixed projects and added to json file

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,11 +18,11 @@ const Projects = () => {
 
     const ProjCard = ({ title, tech, image, description, link }) => (
         <ProjAnchor href={link} target='_blank'>
-            <ProjCardWrapper mw='270px'>
-                <Text bold size='1.5rem' lh='0'>{title}</Text>
+            <ProjCardWrapper mw='270px' style={{ textAlign: 'left'}}>
+                <Text bold size='1.4rem' lh='0'>{title}</Text>
                 <Text>🛠 {tech}</Text>
-                <img src={image} alt={title} width="270px" height="200px" style={{display: 'block', margin: 'auto', flexBasis: '90%'}} />
-                <div style={{ display: 'flex' }}>
+                <img src={image} alt={title} width="270px" height="150px" style={{display: 'block', margin: 'auto', flexBasis: '90%'}} />
+                <div style={{ display: 'flex', marginTop: '10px' }}>
                     <Text>📔</Text>
                     <Text lh='1.5'>{description}</Text>
                 </div>         
@@ -36,9 +36,11 @@ const Projects = () => {
             <Nav />
             <HeaderSpace />
             <Text bold size='3rem' style={{ textAlign: 'center'}}>projects 👨🏻‍💻</Text>
-            {projectList.map(project => (
-                <ProjCard key={project.id} link={project.link} title={project.title} image={project.image} tech={project.tech} description={project.description} />
-            ))}            
+            <div style={{ textAlign: 'center'}}>
+                {projectList.map(project => (
+                    <ProjCard key={project.id} link={project.link} title={project.title} image={project.image} tech={project.tech} description={project.description} />
+                ))}
+            </div>            
         </>
     )
 }
