f597ddaa21bc1f891f0efd2d957761b78c0aacce
Justin Zhang <judtin@seas.upenn.edu>
2022-01-11 23:59:45 -0500

    changed about

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,6 @@ import Nav from './Nav'
 import Footer from './Footer'
 import { HeaderSpace, Main, Text, Row } from './Styles'
 
-
-
 const About= () => {
     return (
         <>
@@ -12,7 +10,7 @@ const About= () => {
             <HeaderSpace />
             <Main>
                 <Row>
-                <Text bold size='3rem' style={{ textAlign: 'center'}}>projects 👨🏻‍💻</Text>
+                {/* <Text bold size='3rem' style={{ textAlign: 'center'}}>projects 👨🏻‍💻</Text> */}
                 </Row>
                 <div style={{ textAlign: 'center'}}>
                     hi
@@ -23,5 +21,45 @@ const About= () => {
         </>
     )
 }
+
+
+/*
+
+About Me:
+Time to break down that collage from before!
+
+I'm from Holmdel, NJ, now in Philly for college 
+I think Penguins are great
+Software Engineer Intern at MongoDB
+
+Penn:
+Penn Labs
+CIS121 TA
+Wharton VIP-X
+Had the opportunity to take some for fun courses 
+like design, philosophy, capitalism, human computation
+theory based coursework mixed with application-heavy extracirriculars
+
+Technology:
+Java Python (Django, Flask, Pandas, Numpy), Javascript (React, Redux, Next, Express) C
+MongoDB, PostgreSQL, SQLite and many ORM's (Django, SQLAcademy) 
+
+Worked in devops AWS suite and CI/CD for research, little bit of containerization Docker & cloud infrastructure
+Product development and agile
+
+Git, Postman, Figma
+
+
+For-Fun:
+Going out to try out food (my favorite food is pizza)
+Watching food reviews (Gordan ramsay dishes in 10, friend chicken sandwich review, popeyes, and mikey chen)
+Driving around with friends
+board games 
+League of Legends
+Movies and TV Shows
+Music
+
+*/
+
 
 export default About
